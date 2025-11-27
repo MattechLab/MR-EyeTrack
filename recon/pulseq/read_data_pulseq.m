3921567984f59e97b50832cecc0661d1fb413e65
Jaime Barranco <jaimebarran@gmail.com>
2025-11-27 10:41:31 +0100

    rmv: duplicated script for read data in pulseq format

--- recon/pulseq/read_data_pulseq.m
+++ recon/pulseq/read_data_pulseq.m
@@ -6,20 +6,15 @@ addpath(genpath('/Users/cag/Documents/forclone/pulseq_v15'));
 addpath(genpath('/home/debi/yiwei/forclone/pulseq'));
 addpath(genpath('/home/debi/yiwei/forclone/pulseqmreye'));
 
-sequence_path = 'data/2025-11_pulseq-p2p/sequence+prescan/yj_seq2_t1w_libre_main_TR6.2ms_TE3.6ms_swap1_FA6_RF2_mreye_track_trajPTP_44_1872.seq';
+sequence_path = 'data/study/pulseq/yj_seq2_t1w_libre_main_TR6.2ms_TE3.6ms_swap1_FA6_RF2_mreye_track_trajPTP_44_1872.seq';
 
 if ~isfile(sequence_path)
     error('File not found: %s', sequence_path);
 end
 
 %% Display the contents of the .seq file
+% This script opens the file in Matlab
 uiopen(sequence_path,1);
-
-%% Load the sequence using Pulseq MATLAB API
-% if the Pulseq MATLAB API is installed and on path:
-seq = mr.Sequence();
-seq.read(sequence_path);
-disp('Sequence loaded via Pulseq API.');
 
 %% Display sequence definitions if Pulseq API is available
 if exist('mr.Sequence','class')
